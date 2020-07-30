cb93794dd0e6db57cebaaca58ebfff194103b10c
Michael Fiano <mail@michaelfiano.com>
2020-07-30 14:47:54 -0400

    slot-map: use dynamic arrays for slots and reverse-map

--- src/data-structures/slot-map.lisp
+++ src/data-structures/slot-map.lisp
@@ -7,6 +7,7 @@
 
 (defpackage #:net.mfiano.lisp.algae.data-structures.slot-map
   (:local-nicknames
+   (#:da #:net.mfiano.lisp.algae.data-structures.dynamic-array)
    (#:u #:net.mfiano.lisp.golden-utils))
   (:use #:cl)
   (:shadow
@@ -25,14 +26,15 @@
 
 (u:define-constant +version-bits+ 32 :test #'=)
 
+(declaim (inline make-slot-map))
 (defstruct (slot-map
             (:constructor %make-slot-map)
             (:conc-name nil)
             (:predicate nil)
             (:copier nil))
-  data
-  slots
-  reverse-map
+  (data (make-array 0 :adjustable t :fill-pointer 0) :type vector)
+  (slots (da:make-array :capacity (expt 10 7)) :type da:dynamic-array)
+  (reverse-map (da:make-array :capacity (expt 10 7)) :type da:dynamic-array)
   (free-head nil :type (or null u:ub24))
   (free-tail nil :type (or null u:ub24)))
 
@@ -40,14 +42,11 @@
   (format stream "~s" (data slot-map)))
 
 (defun make-slot-map (&key (size 128) (data-type 'u:ub32))
-  (flet ((vec (type)
-           (make-array size
-                       :adjustable t
-                       :fill-pointer 0
-                       :element-type type)))
-    (%make-slot-map :data (vec data-type)
-                    :slots (vec 'fixnum)
-                    :reverse-map (vec 'u:ub24))))
+  (let ((data (make-array size
+                          :adjustable t
+                          :fill-pointer 0
+                          :element-type data-type)))
+    (%make-slot-map :data data)))
 
 (u:fn-> pack (u:ub24 u:ub32) fixnum)
 (u:defun-inline pack (id version)
@@ -73,21 +72,23 @@
   `(setf ,place (pack ,id (version ,place))))
 
 (u:fn-> enqueue-free (slot-map fixnum) null)
-(defun enqueue-free (slot-map key)
+(u:defun-inline enqueue-free (slot-map key)
+  (declare (optimize speed))
   (let ((slots (slots slot-map))
         (tail (free-tail slot-map))
         (id (id key)))
-    (repack-id (aref slots id) id)
+    (repack-id (da:aref slots id) id)
     (setf (free-tail slot-map) id)
     (if tail
-        (repack-id (aref slots tail) id)
+        (repack-id (da:aref slots tail) id)
         (setf (free-head slot-map) id))
     (values)))
 
 (u:fn-> dequeue-free (slot-map) (or u:ub24 null))
-(defun dequeue-free (slot-map)
+(u:defun-inline dequeue-free (slot-map)
+  (declare (optimize speed))
   (u:when-let* ((head (free-head slot-map))
-                (next (id (aref (slots slot-map) head))))
+                (next (id (da:aref (slots slot-map) head))))
     (if (= head next)
         (setf (free-head slot-map) nil
               (free-tail slot-map) nil)
@@ -96,35 +97,38 @@
 
 (u:fn-> insert (slot-map t) fixnum)
 (defun insert (slot-map value)
+  (declare (optimize speed))
   (let* ((data (data slot-map))
          (data-index (length data))
          (slots (slots slot-map))
          (free (dequeue-free slot-map)))
     (vector-push-extend value data)
     (if free
-        (let ((version (version (aref slots free))))
-          (setf (aref slots free) (pack data-index version))
-          (vector-push-extend free (reverse-map slot-map))
+        (let ((version (version (da:aref slots free))))
+          (setf (da:aref slots free) (pack data-index version))
+          (da:push (reverse-map slot-map) free)
           (values (pack free version)))
-        (let ((slot-index (length slots)))
-          (vector-push-extend (pack data-index 0) slots)
-          (vector-push-extend slot-index (reverse-map slot-map))
+        (let ((slot-index (da:length slots)))
+          (da:push slots (pack data-index 0))
+          (da:push (reverse-map slot-map) slot-index)
           (values (pack slot-index 0))))))
 
 (u:fn-> delete (slot-map fixnum) boolean)
 (defun delete (slot-map key)
+  (declare (optimize speed))
   (u:mvlet* ((id version (unpack key))
              (slots (slots slot-map)))
-    (when (< id (length slots))
-      (u:mvlet ((slot-id slot-version (unpack (aref slots id))))
+    (when (< id (da:length slots))
+      (u:mvlet ((slot-id slot-version (unpack (da:aref slots id))))
         (when (= version slot-version)
           (let* ((data (data slot-map))
                  (reverse-map (reverse-map slot-map))
-                 (reverse-index (vector-pop reverse-map)))
-            (setf (aref data slot-id) (vector-pop data)
-                  (aref reverse-map slot-id) reverse-index
-                  (aref slots id) (pack id (1+ (version (aref slots id)))))
-            (repack-id (aref slots reverse-index) slot-id)
+                 (reverse-index (da:pop reverse-map)))
+            (locally (declare (optimize (speed 1)))
+              (setf (aref data slot-id) (vector-pop data)))
+            (setf (da:aref reverse-map slot-id) reverse-index
+                  (da:aref slots id) (pack id (1+ (version (da:aref slots id)))))
+            (repack-id (da:aref slots reverse-index) slot-id)
             (enqueue-free slot-map key)
             t))))))
 
@@ -132,18 +136,20 @@
 (defun find (slot-map key)
   (u:mvlet ((id version (unpack key))
             (slots (slots slot-map)))
-    (when (< id (length slots))
-      (u:mvlet ((slot-id slot-version (unpack (aref slots id))))
+    (when (< id (da:length slots))
+      (u:mvlet ((slot-id slot-version (unpack (da:aref slots id))))
         (when (= version slot-version)
           (aref (data slot-map) slot-id))))))
 
 (u:fn-> map-keys (slot-map function) null)
 (defun map-keys (slot-map func)
   (dotimes (i (length (data slot-map)))
-    (let* ((reverse (aref (reverse-map slot-map) i))
-           (slot (aref (slots slot-map) reverse)))
+    (let* ((reverse (da:aref (reverse-map slot-map) i))
+           (slot (da:aref (slots slot-map) reverse)))
       (funcall func (pack reverse (version slot))))))
 
 (u:fn-> map-values (slot-map function) null)
 (defun map-values (slot-map func)
-  (map nil func (data slot-map)))
+  (let ((data (data slot-map)))
+    (dotimes (i (length data))
+      (funcall func (aref data i)))))
