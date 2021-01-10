8e7ba8a30cb21aae51170afb6973fa326b8361b8
Michael Fiano <mail@mfiano.net>
2021-01-10 06:31:54 -0500

    rng: add condition for invalid-range

--- src/rng/rng.lisp
+++ src/rng/rng.lisp
@@ -1144,6 +1144,18 @@
   (seed 0 :type u:ub64)
   (seed-phrase "" :type string))
 
+(define-condition invalid-range ()
+  ((%lower-bound :reader lower-bound
+                 :initarg :min)
+   (%upper-bound :reader upper-bound
+                 :initarg :max))
+  (:report
+   (lambda (condition stream)
+     (format stream "Invalid range: [min: ~s, max: ~s].~%~
+                     Lower bound can not be larger than upper bound."
+             (lower-bound condition)
+             (upper-bound condition)))))
+
 (defun generate-seed-phrase ()
   (let (words)
     (setf *random-state* (make-random-state t))
